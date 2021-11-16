a323f922ced8525c4676601b76f6f500b3d93506
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-11-16 21:11:16 +0100

    play with string

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,11 @@
+#Complete this source code to print Welcome to Holberton School!
+
+#You are not allowed to use any loops or conditional statements.
+#You have to use the variables str1 and str2 in your new line of code
+#Your program should be exactly 5 lines long
+
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
