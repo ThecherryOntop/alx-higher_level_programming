3b5845e4862028caca8a542d434df0f5d77a55b4
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-11-23 10:36:39 -0800

    All

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,12 +1,3 @@
-#Complete this source code
-
-#You can find the source code here
-#You are not allowed to use any loops or conditional statements
-#Your program should be exactly 8 lines long
-#word_first_3 should contain the first 3 letters of the variable word
-#word_last_2 should contain the last 2 letters of the variable word
-#middle_word should contain the value of the variable word without the first and last letters
-
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
