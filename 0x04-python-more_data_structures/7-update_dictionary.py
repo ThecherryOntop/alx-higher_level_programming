db73a4e139ec170732fcd75629356847af141e6a
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-11-24 06:21:56 -0800

    deletes a key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,6 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    #Replace or add key/value pairs in a dictionary
+    # Replace or add key/value pairs in a dictionary
     a_dictionary[key] = value
     return (a_dictionary)
